78f6b769df372b69876e1a6c766fce5dd0342d44
Pranav Tripathi <tripathipranav14@gmail.com>
2023-06-13 23:14:18 +0530

    Authorisation using next-auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import '@/styles/globals.css'
+import { SessionProvider } from "next-auth/react"
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const App = ({
+  Component, pageProps: { session, ...pageProps }
+}) => {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
+
+export default App
