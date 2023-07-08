3454ed9eb975967b4a83c46e4a467da320e7c27d
Pranav Tripathi <tripathipranav14@gmail.com>
2023-07-08 14:08:51 +0530

    update: add loader while fetching

--- pages/products.js
+++ pages/products.js
@@ -5,15 +5,25 @@ import Link from 'next/link'
 import { useEffect, useState } from "react"
 import axios from "axios"
 import { FaPen, FaTrash } from "react-icons/fa"
+import Loader from "@/components/Loader"
 
 const Products = () => {
     const [products, setProducts] = useState([])
+    const [loading, setLoading] = useState(false)
 
     useEffect(() => {
-        axios.get('/api/products').then(response => {
-            setProducts(response.data)
-        })
-    }, [])
+        setLoading(true);
+        axios.get('/api/products')
+            .then(response => {
+                setProducts(response.data);
+                setLoading(false);
+            })
+            .catch(error => {
+                console.error(error);
+                setLoading(false);
+            });
+    }, []);
+
 
     return (
         <Layout>
@@ -21,6 +31,7 @@ const Products = () => {
                 Add new Products
             </Link>
             <table className="basic">
+                {loading && <Loader />}
                 <thead>
                     <tr>
                         <td>Product Name</td>
